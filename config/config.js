fc6a9bff0f021f3ac31d5a74a3a1e2de80de08a2
JY-Ahn <junyongsw92@gmail.com>
2020-01-09 14:47:07 +0900

    데이터베이스 이름 수정

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: 'junyong',
     password: 'cjdehdls1',
-    database: 'database-2',
+    database: 'shortly_db_rds_deploy',
     port: 3306,
     host: 'database-2.cwhf7t2jlg1w.us-east-2.rds.amazonaws.com',
     dialect: 'mysql',
