d2a2b181051d5790948d2f4cc98c92bd54d1276a
JY-Ahn <junyongsw92@gmail.com>
2020-01-09 22:59:34 +0900

    try1

--- config/config.js
+++ config/config.js
@@ -2,11 +2,11 @@ module.exports = {
   development: {
     username: 'junyong',
     password: 'cjdehdls1',
-    database: 'shortly_db_rds_deploy',
-    port: 3306,
-    host: 'database-2.cwhf7t2jlg1w.us-east-2.rds.amazonaws.com',
+    database: 'shortly_db_deploy',
+    port: 13306,
+    host: 'database-1.cu9pcf3wyzrv.ap-northeast-2.rds.amazonaws.com',
     dialect: 'mysql',
-    logging: false,
-    use_env_variable: true
+    logging: false
+    // use_env_variable: true
   }
 };
