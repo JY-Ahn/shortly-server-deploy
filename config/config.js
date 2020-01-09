4e700f983e697ff64db00bcf42e795db511d0684
JY-Ahn <junyongsw92@gmail.com>
2020-01-09 23:08:40 +0900

    try2

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     username: 'junyong',
     password: 'cjdehdls1',
     database: 'shortly_db_deploy',
-    port: 13306,
+    port: 3306,
     host: 'database-1.cu9pcf3wyzrv.ap-northeast-2.rds.amazonaws.com',
     dialect: 'mysql',
     logging: false
