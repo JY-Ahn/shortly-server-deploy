a092f686951d2ff57733dfd2dce5c804e5582132
JY-Ahn <junyongsw92@gmail.com>
2020-01-09 14:33:13 +0900

    포트번호 숫자로

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     username: 'junyong',
     password: 'cjdehdls1',
     database: 'database-2',
-    port: '3306',
+    port: 3306,
     host: 'database-2.cwhf7t2jlg1w.us-east-2.rds.amazonaws.com',
     dialect: 'mysql',
     logging: false
